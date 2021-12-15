fd4b4a1e939cad70bb905dd2df1436288acf5f15
fadhilelrizanda <fadhilelrizanda@gmail.com>
2021-12-15 14:29:13 +0700

    update error

--- src/components/Layout/Navbar/index.js
+++ src/components/Layout/Navbar/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import logoReact from "/logo192.png";
 
 export default class Navbars extends Component {
   render() {
@@ -10,10 +9,7 @@ export default class Navbars extends Component {
         <>
           <Navbar bg="dark" variant="dark" fixed="top">
             <Container>
-              <Navbar.Brand href="#home">
-                <img src={logoReact} />
-                React-IoT
-              </Navbar.Brand>
+              <Navbar.Brand href="#home">React-IoT</Navbar.Brand>
               <Nav className="me-auto">
                 <Nav.Link href="#">Home</Nav.Link>
                 <Nav.Link href="#">Monitoring</Nav.Link>
